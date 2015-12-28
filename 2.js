e2bfa0c3288454c1a9388b4eaba31a9e4b6af40e
Mandeep <mandeep419.singh@gmail.com>
2015-12-27 22:18:22 -0600

    added read file module

--- 2.js
+++ 2.js
@@ -1,19 +1,15 @@
  function printHelp(){
- 	console.log("1.js (c) Mandeep Singh");
+ 	console.log("2.js (c) Mandeep Singh");
  	console.log("");
  	console.log("usage:");
  	console.log("--help             print this help");
  	console.log("--file={NAME}      read the file of {NAME} and output it");
  	console.log("");
  }
-var args = require("minimist")(process.argv.slice(2),{ string:"file"});
+var args =require("minimist")(process.argv.slice(2),{ string:"file"});
   if(args.help || !args.file){
   	printHelp();
   	process.exit(1);
   }
 
-var hello = require("./helloworld.js")
-hello.say(args.file,function(err ,contents){
-   console.log(contents.toString());
-})
-
+  require("./helloworld.js")
---
+++ helloworld.js
@@ -0,0 +1,5 @@
+function say(filename,cb){
+   return fs.readFile(filename,cb);
+}
+ var fs= require("fs");
+module.exports.say = say;
